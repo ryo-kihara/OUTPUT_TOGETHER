6a425be7afb428be1946ef3af97ca8de596842c9
木原凌 <>
2020-11-01 14:18:26 +0900

    add: ユーザー画面のルート追加とそれに伴うルート定義順の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'login' => 'login#login'
+  get 'register' => 'login#register'
   root 'togetther#index'
   get 'create' => 'togetther#create'
   get 'tweets/:id' => 'togetther#show'
-  get 'login' => 'login#login'
-  get 'register' => 'login#register'
+  get ':id' => 'user#show'
 end
