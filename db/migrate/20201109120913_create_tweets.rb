0c957d6164feb9f9cdd0e84a4b8bd88e60298e4f
Shogo46 <goodohayoume@gmail.com>
2020-11-09 21:56:04 +0900

    add:tweetテーブル追加

--- db/migrate/20201109120913_create_tweets.rb
+++ db/migrate/20201109120913_create_tweets.rb
@@ -2,7 +2,11 @@ class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
 
+      t.integer :user_id, :null => false, index :{unique: true}
+      t.string :contents, :limit => 140, :null => false
       t.timestamps
+      t.datetime :deleted_at, default => null
+    
     end
   end
 end
