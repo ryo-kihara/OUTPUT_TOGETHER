5c752305d833518a15d38fef867b75e00f70fc1b
木原凌 <>
2020-11-12 10:10:43 +0900

    add: usersテーブルに初期データ投入

---
+++ db/seed/user.rb
@@ -0,0 +1,10 @@
+10.times do |n|
+  User.create(
+    name: Faker::Name.name,
+    avatar: Faker::Avatar.image,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    google_user_id: Faker::IDNumber.chilean_id,
+    twitter_user_id: Faker::Twitter.user[:id]
+  )
+end
